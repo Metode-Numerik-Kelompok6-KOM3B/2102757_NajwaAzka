1cb9fd733d96424ae4456750635bb6c161e2afd7
najwaka <119663916+najwaka@users.noreply.github.com>
2023-01-13 13:59:02 +0700

    Update metodeRapshon.py

--- metodeRapshon.py
+++ metodeRapshon.py
@@ -12,7 +12,7 @@ def newtonRaphson(x0,e,N):
     condition = True
     while condition:
         if g(x0) == 0.0:
-            print('Divide by zero error!')
+            print('error!')
             break
         x1 = x0 - f(x0)/g(x0)
         print('Iterasi-%d, x1 = %0.6f dan f(x1) = %0.6f' % (step, x1, f(x1)))
@@ -24,15 +24,15 @@ def newtonRaphson(x0,e,N):
         condition = abs(f(x1)) > e
     
     if flag==1:
-        print('\nRequired root is: %0.8f' % x1)
+        print('\n\akar yang diperlukan: %0.8f' % x1)
         
     else:
-        print('\nNot Convergent.')
+        print('\ntak terdefinisi.')
 
 
-x0 = input('Enter Guess: ')
-e = input('Tolerable Error: ')
-N = input('Maximum Step: ')
+x0 = input('Masukan : ')
+e = input('Masukan jumlah error ')
+N = input('Maksimal langkah: ')
 
 x0 = float(x0)
 e = float(e)
